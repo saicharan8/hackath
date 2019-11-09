c0a55042d27a7fb61f9159a7dd068d371dfddadf
Saicharan Yadavelli <Saicharan.Yadavelli@neudesic.com>
2019-11-09 12:13:02 +0530

    hackathon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'HACKATHON';
+  data: any = [];
+  url = "https://api.myjson.com/bins/huzbg";
+  flagCsharp: boolean = false;
+  cSharpQue: any;
+  flagMvc: boolean = false;
+  mvcQue: any;
+  flagAngular: boolean = false;
+  AngularQue: any;
+  textbox = 1;
+  currentQuestion = {};
+  results = [];
+  suggetionQ = [];
+  total = 0;
+  totalPercantage= 0;
+  constructor(private httpclient: HttpClient) {
+
+  }
+  ngOnInit() {
+    this.httpclient.get(this.url).subscribe(res => {
+      this.data = res;
+      this.cSharpQue = this.data.filter(x => x.TOPIC === "C#");
+      this.mvcQue = this.data.filter(x => x.TOPIC === "WEBAPI");
+      this.AngularQue = this.data.filter(x => x.TOPIC === "ANGULAR");
+    });
+  }
+  language(lang: any) {
+    if (lang === "C#") {
+      this.flagCsharp = true;
+      this.currentQuestion = this.cSharpQue.filter(x => x.LEVEL == this.textbox && x.ASKED === 0)[0];
+    }
+    if (lang === "WEBAPI") {
+      this.flagMvc = true;
+      this.currentQuestion = this.mvcQue.filter(x => x.LEVEL == this.textbox && x.ASKED === 0)[0];
+      
+    }
+    if (lang === "ANGULAR") {
+      this.flagAngular = true;
+      this.currentQuestion = this.AngularQue.filter(x => x.LEVEL == this.textbox && x.ASKED === 0)[0];
+      
+    }
+  }
+
+  rating(level) {
+    this.currentQuestion['ASKED'] = 1;
+    this.textbox = level;
+    this.language(this.currentQuestion['TOPIC']);
+    var obj = {Question:'', Topic:'', Rating:0, SUGGESTION:'' };
+    obj.Question =this.currentQuestion['QUESTION'];
+    obj.Topic =this.currentQuestion['TOPIC'];
+    obj.Rating =level;
+    
+    this.results.push(obj);
+    console.log(this.results);
+  }
+  complete(){
+    this.results.forEach(element => {
+      this.total = this.total + element.Rating
+       this.totalPercantage = this.total/(this.results.length*5);
+     });
+  }
 }
